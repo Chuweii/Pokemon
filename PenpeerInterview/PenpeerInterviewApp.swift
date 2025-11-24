280c120b1b42940f92a8cffdd98588d9fae81c9c
Weber <90306126+Chuweii@users.noreply.github.com>
2025-11-24 14:20:38 +0800

    Initial commit

---
+++ PenpeerInterview/PenpeerInterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenpeerInterviewApp.swift
+//  PenpeerInterview
+//
+//  Created by Wei Chu on 2025/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PenpeerInterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PenpeerInterviewTests/PenpeerInterviewTests.swift
@@ -0,0 +1,17 @@
+//
+//  PenpeerInterviewTests.swift
+//  PenpeerInterviewTests
+//
+//  Created by Wei Chu on 2025/11/24.
+//
+
+import Testing
+@testable import PenpeerInterview
+
+struct PenpeerInterviewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
