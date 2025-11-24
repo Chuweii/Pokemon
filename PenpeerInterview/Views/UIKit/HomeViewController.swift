2c9f1cab6593d5df86e02ab488c2dd2882d30052
Weber <90306126+Chuweii@users.noreply.github.com>
2025-11-24 15:19:03 +0800

    feat: Setup library and file
    - Add moya, snapkit library
    - Add HomeViewController

---
+++ PenpeerInterview/Views/UIKit/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  PenpeerInterview
+//
+//  Created by Wei Chu on 2025/11/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
